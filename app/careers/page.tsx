a6019df1e92773bed2fef818c61707347bb4da34
Sujal <palsujal707@gmail.com>
2026-01-12 19:25:48 +0530

    added items in career page

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -3,6 +3,7 @@ import CareerHero from "@/components/careers/CareerHero"
 import Footer from "@/components/footer"
 import Navbar from "@/components/navbar"
 import Newsletter from "@/components/newsletter"
+import WhoWe from "@/components/whowe"
 
 
 export default function CareersPage() {
@@ -10,6 +11,7 @@ export default function CareersPage() {
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <CareerHero/>
+      <WhoWe />
       <CallToAction />
       <Newsletter />
       <Footer />
