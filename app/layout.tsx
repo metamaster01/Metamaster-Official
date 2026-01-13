0551a288e9f0fe641dda4e4f3885739fcb235252
Sujal <palsujal707@gmail.com>
2026-01-13 22:17:34 +0530

    fix word

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AppWrapper from "@/components/AppWrapper";
+import AppWrapper from "@/components/AppWraper";
 import { Analytics } from '@vercel/analytics/next';
 
 
